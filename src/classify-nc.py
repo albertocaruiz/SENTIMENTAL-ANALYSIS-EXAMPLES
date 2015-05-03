d5448b915bf3ee2b468f65630f636f61bf16c3e6
sophia yan <sophiaxueting@gmail.com>
2015-05-03 16:38:19 -0500

    data processing

--- src/classify-nc.py
+++ src/classify-nc.py
@@ -9,23 +9,33 @@ def evaluation(predication, ground_truth):
 			corr+=1
 
 	print "Accuracy: ", corr/len(predication)
+	print "Wordcount has been normalized by the maxlength of the sentence",maxlength;
  
-negative = open("/Users/sophiayan/Documents/cs410_GroupProject/SentimentalAnalysis/vector/positive.txt","r");
-positive = open("/Users/sophiayan/Documents/cs410_GroupProject/SentimentalAnalysis/vector/negative.txt","r");
+positive = open("../vector/positive.txt","r");
+negative = open("../vector/negative.txt","r");
 
 all_instances = [];
+maxlength = 0;
 
 for row1 in positive:
 	row1 = row1[:-1]
 	row1 = row1.split(',')
+	print row1
 	row1 = [int(i) for i in row1]
 	all_instances.append(row1)
+	if(row1[0] > maxlength):
+	    maxlength = row1[0];
 
 for row2 in negative:
 	row2 = row2[:-1]
 	row2 = row2.split(',')
 	row2 = [int(i) for i in row2]
 	all_instances.append(row2)
+	if(row2[0] > maxlength):
+	    maxlength = row2[0];
+
+for instance in all_instances:
+    instance[0] = instance[0]/maxlength; 
 	
 random.shuffle(all_instances)
 # print all_instances[0:700]
